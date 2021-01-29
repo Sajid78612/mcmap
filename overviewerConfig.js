efc7a562e76f6e2fe29f7a4af339f60a6ebbfcc0
Sajid <sajidalam786@gmail.com>
2021-01-29 21:29:38 +0000

    Update 29/01

--- overviewerConfig.js
+++ overviewerConfig.js
@@ -27,7 +27,7 @@ var overviewerConfig = {
     ],
     "map": {
         "debug": false,
-        "cacheTag": "1611605958",
+        "cacheTag": "1611955757",
         "north_direction": "lower-left",
         "controls": {
             "pan": true,
@@ -49,7 +49,7 @@ var overviewerConfig = {
             "base": "",
             "bgcolor": "#1a1a1a",
             "world": "world",
-            "last_rendertime": 1611605925,
+            "last_rendertime": 1611955568,
             "imgextension": "png",
             "isOverlay": false,
             "poititle": "Markers",
